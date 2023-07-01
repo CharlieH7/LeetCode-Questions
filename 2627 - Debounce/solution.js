5752ce94e405d30409733af59691b6b17859ad68
CharlieH7 <hope91824@gmail.com>
2023-07-02 00:01:57 +1000

    solution for Debounce 2627

---
+++ 2627 - Debounce/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Function} function 
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+
+var debounce = function(fn, t) {
+    let id;
+    return function(...args) {
+        clearTimeout(id);
+        id = setTimeout(() => fn(...args), t);
+    }
+};
